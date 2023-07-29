678d11633cb33d92fce8c2da37db2cdc99758c2a
Kishoreganesh Sundararajan <kishore7403@gmail.com>
2023-07-29 06:09:10 -0300

    apis changeg and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/login';
+import SignUpPage from './pages/signUp';
+import HomePage from './pages/home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage/>} />
+        {/* <Route path="/signup" element={<SignupPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
